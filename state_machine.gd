abcd69d402d90bbd6b743963af404e53fb6ab3c0
Luke <gamersloop@gmail.com>
2017-12-19 02:21:39 -0500

    tweak variable names

--- state_machine.gd
+++ state_machine.gd
@@ -1,8 +1,8 @@
 # Finite State machine
 extends Node
 
-var last_state setget , get_last_state
-var current_state setget set_current_state, get_current_state
+var __last_state setget __set_last_state, get_last_state
+var __current_state setget set_current_state, get_current_state
 
 func _ready():
 	set_process_input(true)
@@ -12,52 +12,52 @@ func _ready():
 func _input(event):
 	call_deferred("global_input", event)
 	
-	if current_state:
-		if has_method(current_state + "_input"):
-			call_deferred(current_state + "_input", event)
+	if __current_state:
+		if has_method(__current_state + "_input"):
+			call_deferred(__current_state + "_input", event)
 	
 func _process(delta):
 	call_deferred("global_pre_update")
 	
-	if current_state:
-		if has_method(current_state + "_update"): 
-			call_deferred(current_state + "_update")
+	if __current_state:
+		if has_method(__current_state + "_update"): 
+			call_deferred(__current_state + "_update")
 		
 	call_deferred("global_post_update")
 	
 func _fixed_process(delta):
 	call_deferred("global_pre_fixed_update")
 	
-	if current_state:
-		if has_method(current_state + "_fixed_update"): 
-			call_deferred(current_state + "_fixed_update")
+	if __current_state:
+		if has_method(__current_state + "_fixed_update"): 
+			call_deferred(__current_state + "_fixed_update")
 		
 	call_deferred("global_post_fixed_update")
 	
 func set_current_state(new_state):
-	if current_state == new_state: return
+	if __current_state == new_state: return
 	
 	call_deferred("global_exit")
-	if current_state:
-		if has_method(current_state + "_exit"): 
-			call_deferred(current_state + "_exit")
+	if __current_state:
+		if has_method(__current_state + "_exit"): 
+			call_deferred(__current_state + "_exit")
 	
-	last_state = current_state
-	current_state = new_state
+	__last_state = __current_state
+	__current_state = new_state
 	
 	call_deferred("global_enter")
-	if current_state:
-		if has_method(current_state + "_enter"): 
-			call_deferred(current_state + "_enter")
+	if __current_state:
+		if has_method(__current_state + "_enter"): 
+			call_deferred(__current_state + "_enter")
 	
 func get_current_state():
-	return current_state
+	return __current_state
 	
-func set_last_state():
+func __set_last_state():
 	pass
 	
 func get_last_state():
-	return last_state
+	return __last_state
 	
 func global_input(event):
 	pass
