171a8261c257312224cb5ee09976736adc97195c
Luke <gamersloop@gmail.com>
2017-12-19 11:37:19 -0500

    Add new methods and Change process events to use delta
    - Add _unhandled_input
    - Add _unhandled_key_input
    - Change _process and _fixed_process to use delta
    - Change all methods to be optional
    - Change set_[event] to be used by user, not default
    - Remove abstract method bodies

--- state_machine.gd
+++ state_machine.gd
@@ -1,43 +1,68 @@
 # Finite State machine
 extends Node
 
+# Events
+# ------
+# global_input -> [state]_input
+# global_unhandled_input -> global_unhandled_key_input -> [state]_unhandled_input
+# global_pre_fixed_process -> [state]_fixed_process -> global_post_fixed_process
+# global_pre_process -> [state]_process -> global_post_process
+
 var __last_state setget __set_last_state, get_last_state
 var __current_state setget set_current_state, get_current_state
-
-func _ready():
-	set_process_input(true)
-	set_process(true)
-	set_fixed_process(true)
 	
 func _input(event):
-	call_deferred("global_input", event)
+	if has_method("global_input"):
+		call_deferred("global_input", event)
 	
 	if __current_state:
 		if has_method(__current_state + "_input"):
 			call_deferred(__current_state + "_input", event)
 	
+func _unhandled_input(event):
+	if has_method("global_unhandled_input"):
+		call_deferred("global_unhandled_input", event)
+		
+	if __current_state:
+		if has_method(__current_state + "_unhandled_input"):
+			call_deferred(__current_state + "_unhandled_input", event)
+			
+func _unhandled_key_input(key_event):
+	if has_method("global_unhandled_key_input"):
+		call_deferred("global_unhandled_key_input", key_event)
+		
+	if __current_state:
+		if has_method(__current_state + "_unhandled_key_input"):
+			call_deferred(__current_state + "_unhandled_key_input", key_event)
+	
 func _process(delta):
-	call_deferred("global_pre_update")
+	if has_method("global_pre_update"):
+		call_deferred("global_pre_update", delta)
 	
 	if __current_state:
 		if has_method(__current_state + "_update"): 
-			call_deferred(__current_state + "_update")
+			call_deferred(__current_state + "_update", delta)
 		
-	call_deferred("global_post_update")
+	if has_method("global_post_update"):
+		call_deferred("global_post_update", delta)
 	
 func _fixed_process(delta):
-	call_deferred("global_pre_fixed_update")
+	if has_method("global_pre_fixed_update"):
+		call_deferred("global_pre_fixed_update", delta)
 	
 	if __current_state:
 		if has_method(__current_state + "_fixed_update"): 
-			call_deferred(__current_state + "_fixed_update")
-		
-	call_deferred("global_post_fixed_update")
+			call_deferred(__current_state + "_fixed_update", delta)
+	
+	if has_method("global_post_fixed_update"):
+		call_deferred("global_post_fixed_update", delta)
 	
 func set_current_state(new_state):
 	if __current_state == new_state: return
 	
-	call_deferred("global_exit")
+	if has_method("global_exit"):
+		call_deferred("global_exit")
+	
 	if __current_state:
 		if has_method(__current_state + "_exit"): 
 			call_deferred(__current_state + "_exit")
@@ -45,7 +70,9 @@ func set_current_state(new_state):
 	__last_state = __current_state
 	__current_state = new_state
 	
-	call_deferred("global_enter")
+	if has_method("global_enter"):
+		call_deferred("global_enter")
+	
 	if __current_state:
 		if has_method(__current_state + "_enter"): 
 			call_deferred(__current_state + "_enter")
@@ -58,24 +85,3 @@ func __set_last_state():
 	
 func get_last_state():
 	return __last_state
-	
-func global_input(event):
-	pass
-	
-func global_enter():
-	pass
-	
-func global_exit():
-	pass
-
-func global_pre_update():
-	pass
-	
-func global_post_update():
-	pass
-	
-func global_pre_fixed_update():
-	pass
-	
-func global_post_fixed_update():
-	pass
